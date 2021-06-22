e9aa336fc929eff90b5c198ad62c5a575da39d5c
Willedunn4 <82042225+Willedunn4@users.noreply.github.com>
2021-06-22 16:13:46 -0500

    Add files via upload

--- Index.js
+++ Index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 4000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('How ya doin today?'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
