b4691649b10227603b05056c0063b7f8cb754828
Willedunn4 <82042225+Willedunn4@users.noreply.github.com>
2021-06-23 14:27:39 -0500

    Add files via upload

--- Index.js
+++ Index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => res.send('How ya doin today?'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
-<script ;src="https://www.gstatic.com/firebasejs/8.6.8/firebase-app.js"></script>
+<script ;src="https://www.gstatic.com/firebasejs/8.6.8/firebase-app.js">'hello'</script>
 
 <script src="https://www.gstatic.com/firebasejs/8.6.8/firebase-analytics.js"></script>
 
@@ -23,4 +23,65 @@ var firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
 
+
+  noteForm.addEventListener('submit', (e)=>{
+    addNote(e);
+});
+
+  function addNote(e){
+    e.preventDefault();
+    
+
+    let newNote = {};
+
+    let title = document.querySelector('.title');
+    let note = document.querySelector('.note');
+
+    if(title.value == '' || note.value ==''){
+        return alert('Please enter both fields');
+    }else{
+        newNote.title = title.value
+        newNote.note = note.value
+    }
+    
+    title.value = '';
+    note.value = '';
+
+    noteList.push(newNote);
+    appendNotes();
+    cancel.click();
+
+}
+function appendNotes(){
+  let notes = Array.from(document.querySelectorAll('.noteItem'));
+  if(notes.length > 0){
+      notes.forEach(note =>{
+          note.remove();
+      })
+  }
+
+  noteList.map(note =>{
+      // Create table cells
+      let tr = document.createElement('tr');
+      tr.classList = 'noteItem';
+      let tdTitle = document.createElement('td');
+      tdTitle.innerText = note.title;
+      let tdNote = document.createElement('td');
+      tdNote.innerText = note.note;
+      let tdDelete = document.createElement('td');
+      tdDelete.innerHTML = '&times';
+      tdDelete.classList.add('delete-item');
+
+      // Append cells to table row
+      tr.appendChild(tdTitle);
+      tr.appendChild(tdNote);
+      tr.appendChild(tdDelete);
+
+      // Append row to table
+
+      noteTable.appendChild(tr);
+      getDeleteButtons();
+      localStorage.setItem('notes',JSON.stringify(noteList));
+  })
+}
   
