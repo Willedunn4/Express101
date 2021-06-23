c84f860f006b8fbce344f966bb318bf809c7746a
Willedunn4 <82042225+Willedunn4@users.noreply.github.com>
2021-06-23 15:33:21 -0500

    Update Index.js

--- Index.js
+++ Index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 4000
 
-app.get('/', (req, res) => res.send('How ya doin today?'))
+app.get('/', (req, res) => res.send('Hope this works'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
